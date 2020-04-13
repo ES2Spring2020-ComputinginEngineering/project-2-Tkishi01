9d78178e7c72dd8ec235376ee9743f982cfc10e4
Tkishi01 <taylor.kishinami@tufts.edu>
2020-04-12 17:27:51 -1000

    working code but uncommented

--- KMeansClustering_driver.py
+++ KMeansClustering_driver.py
@@ -1,13 +1,53 @@
 #Please place your FUNCTION code for step 4 here.
 import KMeansClustering_functions as kmc #Use kmc to call your functions
 import numpy as np
-x = np.array([4,5,6])
-y = np.array([1,2,3])
-bob = np.array(list(map(lambda i: [y[i],x[i]], range(len(x)))))
-z = np.array([7,8,9])
-chad = np.array(list(map(lambda i: [bob[i],z[i]], range(len(z)))))
-print(bob)
 
-jon = np.column_stack((y,x))
-sam = np.column_stack((jon,z))
-print(sam[0])
+glucose, hemoglobin, classification = kmc.openckdfile()
+glucose, hemoglobin = kmc.normalizeData(glucose, hemoglobin)  
+k=2
+centroidPoints = kmc.selectCentroids(k) 
+classificationList = kmc.assignClusters(k, centroidPoints,glucose,hemoglobin)
+updatedCentroids = kmc.updateCentroids(glucose,hemoglobin,classificationList,k)
+kmc.graphingKMeans(glucose,hemoglobin,centroidPoints,classificationList)
+iterations = 0
+while iterations < 10:
+    classificationList = kmc.assignClusters(k, centroidPoints, glucose, hemoglobin)
+    centroidPoints = kmc.updateCentroids(glucose, hemoglobin, classificationList, k)
+    print("working...")
+    iterations = iterations +1
+kmc.graphingKMeans(glucose, hemoglobin, centroidPoints, classificationList)
+print("Final Centroid Points:")
+print(centroidPoints)
+tP = 0
+fP = 0
+tN = 0
+fN = 0
+ckdCluster = 0
+nonckdCluster = 0
+if np.mean(classificationList) >0.5:
+    hasckd = 1
+    nockd = 0
+else: 
+    hasckd = 0
+    nockd = 1
+for i in range(0,len(classification)):
+    if classification[i] == 0:
+        nonckdCluster = nonckdCluster+1
+        if classificationList[i] == nockd:
+            fN = fN+1
+        if classificationList[i] == hasckd:
+            tN = tN+1
+    if classification[i] == 1:
+        ckdCluster = ckdCluster+1
+        if classificationList[i] == nockd:
+            tP=tP+1
+        if classificationList[i] == hasckd:
+            fP = fP+1
+truePositives = tP/ckdCluster*100
+falsePositives = fP/ckdCluster*100
+trueNegatives = tN/nonckdCluster*100
+falseNegatives = fN/nonckdCluster*100
+print("True Positives:" + str(truePositives))
+print("False Positives:" + str(falsePositives))
+print("True Negatives:" + str(trueNegatives))
+print("False Negatives:" + str(falseNegatives))
--- KMeansClustering_functions.py
+++ KMeansClustering_functions.py
@@ -4,7 +4,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 
-
 def openckdfile():
     glucose, hemoglobin, classification = np.loadtxt('ckd.csv', delimiter=',', skiprows=1, unpack=True)
     return glucose, hemoglobin, classification
@@ -29,25 +28,21 @@ def selectCentroids(k):
     return  np.array(centroidPoints)
 
 def calculateDistanceArray(centroidGlucose,centroidHemoglobin, glucose, hemoglobin):
-    a = (centroidGlucose - glucose)
-    b = (centroidHemoglobin - hemoglobin)
+    a = (centroidHemoglobin - glucose)
+    b = (centroidGlucose - hemoglobin)
     distance = np.sqrt((a**2+b**2))
     return  distance
 
-'''def createDistanceArray(k, centroidPoints, glucose, hemoglobin):
-    distanceArray = np.zeros([158,k])
-    for i in range(158):
-        for k in range(0,len(centroidPoints)):
-            distance = calculateDistance(centroidPoints[k][0], centroidPoints[k][1], glucose[i], hemoglobin[i])
-            np.insert(distanceArray,[i,k],distance)
-    return distanceArray'''
-    
-def assignClusters(k, centroidPoints, glucose, hemoglobin):
+def createDistanceArray(k, centroidPoints, glucose, hemoglobin):
     distanceList=calculateDistanceArray(centroidPoints[0][0], centroidPoints[0][1],glucose,hemoglobin)
-    classificationList = []
     for i in range(1,k):
         distance = calculateDistanceArray(centroidPoints[i][0], centroidPoints[i][1],glucose,hemoglobin)
         distanceList = np.column_stack((distanceList,distance))
+    return distanceList
+
+def assignClusters(k, centroidPoints, glucose, hemoglobin):
+    classificationList = []
+    distanceList = createDistanceArray(k,centroidPoints, glucose, hemoglobin)
     for i in distanceList:
         clusterClass=np.argmin(i)
         classificationList.append(clusterClass)
@@ -59,7 +54,7 @@ def updateCentroids(glucose, hemoglobin, classificationList, k):
         glucoseCentroid = np.mean(glucose[classificationList==i])
         hemoglobinCentroid = np.mean(hemoglobin[classificationList==i]) 
         #print(glucoseCentroid,hemoglobinCentroid)
-        updatedCentroids[i]= np.append(glucoseCentroid,hemoglobinCentroid)
+        updatedCentroids[i]= np.append(hemoglobinCentroid,glucoseCentroid)
     return updatedCentroids
 
 def graphingKMeans(glucose, hemoglobin, centroidPoints, classificationList):
@@ -74,20 +69,6 @@ def graphingKMeans(glucose, hemoglobin, centroidPoints, classificationList):
     plt.show()
 
 ######
-k=3
-glucose, hemoglobin, classification = openckdfile()
-glucose, hemoglobin = normalizeData(glucose, hemoglobin)  
-centroidPoints = selectCentroids(k) 
-classificationList = assignClusters(k, centroidPoints,glucose,hemoglobin)
-updatedCentroids = updateCentroids(glucose,hemoglobin,classificationList,k)
-#print(glucose[classificationList==0])
-#print(centroidPoints)
-#print("....")
-#print(distance)
-#print (centroidPoints[0][1])
-#print(assignClusters(centroidPoints, glucose, hemoglobin))
-graphingKMeans(glucose,hemoglobin,centroidPoints,classificationList)
-#print(centroidPoints[0,0], centroidPoints[0,1])
-print(centroidPoints)
-#print(distanceArray)
-#print(calculateDistance(centroidPoints[0][0], centroidPoints[0,1],glucose,hemoglobin))
+
+
+        
